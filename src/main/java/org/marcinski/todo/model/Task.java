a1e2887962fc7f676c5651f060c22c65ea47157d
Marek Marciński <marcinski.mar@gmail.com>
2018-10-16 18:13:50 +0200

    @Data has been removed, @Getter, @Setter, @ToString annotation has been added, UUID has added for recognition Tasks, Constructor with fixed data declared

--- src/main/java/org/marcinski/todo/model/Task.java
+++ src/main/java/org/marcinski/todo/model/Task.java
@@ -1,13 +1,25 @@
 package org.marcinski.todo.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
 public class Task {
+    private UUID uuid;
     private String description;
     private LocalDateTime addingDateTime;
     private boolean done;
     private TaskPriority priority;
+
+    public Task() {
+        this.uuid = UUID.randomUUID();
+        this.addingDateTime = LocalDateTime.now();
+        this.done = false;
+    }
 }
